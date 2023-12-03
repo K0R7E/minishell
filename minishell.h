95fefd8b75c689053c6d2763cf60866d7eb29888
Aron Kortvelyessy <akortvel@c4r2p9.42vienna.com>
2023-12-03 12:22:59 +0100

    safety save (parsing)

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: akortvel <akortvel@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 18:52:32 by akortvel          #+#    #+#             */
-/*   Updated: 2023/12/01 17:33:34 by akortvel         ###   ########.fr       */
+/*   Updated: 2023/12/03 12:12:52 by akortvel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@
 
 typedef struct s_lexer
 {	
-	int				command;
+	int				pipes;
 	int				type;
 	char			*token;
 	struct s_lexer	*next;
@@ -45,15 +45,15 @@ typedef struct s_lexer_pos
 
 typedef struct s_parsing
 {
-	char *cmd_path;
-	int	command; //whatever this means?
-	t_lexer lexer;
-	char **args;
-	char *in_file;
+	char	*cmd_path;
+	int		command; //whatever this means?
+	t_lexer	lexer;
+	char	**args;
+	char	*in_file;
 	char	*out_file;
-	int	fd_in;
-	int	fd_out;
-	int	fd_pipe[2];
+	int		fd_in;
+	int		fd_out;
+	int		fd_pipe[2];
 }	t_parsing;
 
 typedef struct s_info
--- parsing.c
+++ parsing.c
@@ -3,10 +3,38 @@
 void ft_parser(t_info *info, t_parsing *pars, t_lexer *lexer)
 {
 	int i = 0;
+	int j = 0;
 
-	while (lexer->token[i])
+	while (lexer->next)
 	{
-		
+		if (lexer->type == 1)
+		{
+			pars->args[j] = ft_strdup(lexer->token);
+			printf("args: %s\n", pars->args[j]);
+		}
+		else if (lexer->type == 2)
+		{
+			pars->in_file = ft_strdup(lexer->token);
+			pars->fd_in = open(pars->in_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			printf("redir_in: %s\n", pars->in_file);
+		}
+		else if (lexer->type == 3)
+		{
+			pars->out_file = ft_strdup(lexer->token);
+			pars->fd_out = open(pars->out_file, O_RDONLY);
+			printf("redir_out: %s\n", pars->out_file);
+		}
+		else if (lexer->type == 4)
+		{
+			pars->in_file = ft_strdup(lexer->token);
+			printf("recir_append: %s\n", pars->in_file); // should redirect output in append mode.
+		}
+		else if (lexer->type == 5)
+		{
+			pars->out_file = ft_strdup(lexer->token);
+			printf("heredoc: %s\n", pars->out_file); // heredoc + delimiter
+		}
+		lexer = lexer->next;
 	}
 }
 
