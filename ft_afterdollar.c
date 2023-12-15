ac6b1fc04ce1a31bf89f8a02f9ed00d98a88ed8b
K0R7E <125017875+K0R7E@users.noreply.github.com>
2023-12-15 18:45:00 +0100

    commented $(spec char) what does...

--- ft_afterdollar.c
+++ ft_afterdollar.c
@@ -24,17 +24,26 @@ char *ft_replace(char *str, char c, char k)
 	j = 0;
 	while (str[i])
 	{
-		if ((c != 0) && (str[i] == c))
-		{
-			if ((k != 0) && (str[i + 1] == k))
-			{
-
-			}
+		if (str[i] == c)
 			j++;
-		}
 		str[i] = str[j];
 		i++;
+		j++;
 	}
+	i = 0;
+	j = 0;
+	if (k)
+	{
+		while (str[i])
+		{
+			if (str[i] == k)
+				j++;
+			str[i] = str[j];
+			i++;
+			j++;
+		}
+	}
+	str[i] = '\0';
 	return (str);
 }
 
@@ -42,59 +51,91 @@ char *after_dollar(char *str, t_info *info)
 {
 	int i;
 	int j;
-	char *var;
 
 	(void)info;
 	i = 0;
 	j = 1;
-	var = malloc(sizeof(char) * 100);
 	if (str[i] == '$')
 	{
 		i++;
 		if ((str[i] == '\'' && str[ft_strlen(str) - 1] == '\'') || (str[i] == '\"' && str[ft_strlen(str) - 1] == '\"'))
 			return (str);
-		else if(str[i] == ':' || str[i] == '/' || str[i] == '.' || str[i] == '=' || str[i] == '+' || str[i] == '%' || str[i] == ',')
+		
+		else if(str[i] == '/' || str[i] == '.' || str[i] == '=' || str[i] == '%' || str[i] == ',')
 			return (str);
+		else if (str[i] == ':')
+		{
+			//$: >> In Bash, the dollar sign ($) followed by a colon (:) has a specific meaning when used in parameter expansion. It is used for providing default values for variables that may not be set.
+		}
+		else if (str[i] == '+')
+		{
+			//used in parameter expansion to check if a variable is set (has a value) and, if so, return an empty string. It's often used in conditional statements to check for the existence of a variable.
+		}
 		else if (str[i] == '_')
 		{
-			free(str);
-			str[0] = '\0';
+			//represents the last argument of the last command that was executed.
 		}
 		else if (str[i] == '\\')
 		{
-			strcpy(var, "$");
-    		strcat(var, str); // - $'\\'
-			return (var);
+			//It looks like there's a misunderstanding in your question. In Bash, the special variable $$ (double dollar sign)
+			//represents the process ID (PID) of the current shell. It is used to obtain a unique identifier for the shell process.
+			//On the other hand, the character \ (backslash) is an escape character in Bash. It is used to escape special characters, 
+			//preventing them from being interpreted in their usual way.
+			str = ft_replace(str, '\\', 0);
+			return (str);
 		}
 		else if (str[i] == ';')
 		{
+			//
 			str = ft_strcpy(str, "$");
 		}
 		else if (str[i] == '@')
 		{
-			
+			//represents all the positional parameters (arguments) passed to a script or function.
+			str = ft_replace(str, '@', '$');
+			return (str);
 		}
 		else if (str[i] == '#')
 		{
-
+			//this giv back the number of arguments
 		}
 		else if (str[i] == '0')
 		{
-			
+			// represents the name of the script or shell itself. When used in a script, it provides the name of the script being executed.
+			str = ft_replace(str, '0', '$');
+			str = ft_strjoin("/bin/bash", str);
+			return (str);
 		}
 		else if (ft_isdigit(str[i]))
 		{
+			//represent command-line arguments passed to a script or a function. first argument is represented by $1, second argument is represented by $2 and so on.
 			while ((j != str[i] - '0') && j < 10)
 				j++;
 		}
 		else if (str[i] == '-')
 		{
-			
+			//represents the current options (flags) that are set for the shell. 
+			str = ft_replace(str, '-', '$');
+			str = ft_strjoin("himBH", str);
+			return (str);
+		}
+		else if (str[i] == '!')
+		{
+			//represents the process ID (PID) of the last background command that was executed.
+		}
+		else if (str[i] == '$')
+		{
+			//represents the process ID (PID) of the current shell
 		}
 		else if (str[i] == '?')
 		{
-
+			//represents the exit status of the last executed command.
 		}
+		else if (str[i] == '*')
+		{
+			//represents all the positional parameters (arguments) passed to a script or function as a single string.
+		}
+
 	}
 	return (str);
 }
